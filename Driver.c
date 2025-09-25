540fceb90d78d1a27fac29437bc844268c403bf3
bilibiliLXZY <101698428+bilibiliLXZY@users.noreply.github.com>
2025-09-25 16:48:47 +0800

    Add files via upload

--- Driver.c
+++ Driver.c
@@ -4,9 +4,6 @@
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 	_disable();
 	__writecr8(HIGH_LEVEL);
-	UINT64 cr0 = __readcr0();
-	cr0 &= 0xfffffffffffeffff;
-	__writecr0(cr0);
 	__halt();
 	return STATUS_SUCCESS;
 }
