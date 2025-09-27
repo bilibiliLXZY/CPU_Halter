78a90ba7a9451da935d7e4de67abe920b1dff96b
bilibiliLXZY <101698428+bilibiliLXZY@users.noreply.github.com>
2025-09-27 17:09:43 +0800

    Add files via upload

--- Driver.c
+++ Driver.c
@@ -4,12 +4,13 @@
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
 	ULONG apicBase = (ULONG)(__readmsr(0x1B) & 0xFFFFF000);
 	volatile ULONG* lapic = MmMapIoSpace(MmGetPhysicalAddress((PVOID)apicBase), PAGE_SIZE, MmNonCached);
-	ULONG lvt = lapic[0x320 / 4];
-	lvt = (lvt & ~0x700) | (0x4 << 8);
-	lvt &= ~(1 << 16);
-	lapic[0x320 / 4] = lvt;
-	_disable();
-	__writecr8(HIGH_LEVEL);
+	if (lapic) {
+		ULONG lvt = lapic[0x320 / 4];
+		lvt |= (1 << 16);
+		lapic[0x320 / 4] = lvt;
+		MmUnmapIoSpace((PVOID)lapic, PAGE_SIZE);
+	}
+	__writecr8(26);
 	for(;;)__halt();
 	return STATUS_FAILED_DRIVER_ENTRY;
 }
